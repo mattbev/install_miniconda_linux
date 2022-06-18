38899c4cde785d01a880167b2648e1f966d0acfe
Matthew Beveridge <matt@nodarsensor.com>
2022-06-18 18:03:06 -0400

    remove directory creation

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@
 PROCESSOR_ARCHITECTURE=$(uname -m)
 
 # get files
-mkdir -p ~/miniconda3
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-$PROCESSOR_ARCHITECTURE.sh -O /tmp/miniconda.sh
 bash /tmp/miniconda.sh -b -p ~/miniconda3
 
