2833b3285e5dc71de8508d41aa2608bf0ab9d3a8
Matthew Beveridge <matt@nodarsensor.com>
2022-06-18 17:59:14 -0400

    update paths and args

--- install.sh
+++ install.sh
@@ -5,13 +5,11 @@ PROCESSOR_ARCHITECTURE=$(uname -m)
 
 # get files
 mkdir -p ~/miniconda3
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-$PROCESSOR_ARCHITECTURE.sh -O ~/miniconda3/miniconda.sh
-bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-$PROCESSOR_ARCHITECTURE.sh -O /tmp/miniconda.sh
+bash /tmp/miniconda.sh -b -p ~/miniconda3
 
 # cleanup
-rm -rf ~/miniconda3/miniconda.sh
-~/miniconda3/bin/conda init bash
-~/miniconda3/bin/conda init zsh
+rm /tmp/miniconda.sh
 
 # restart console
 echo 'run "source ~/.bashrc", "source ~/.zshrc", or restart your terminal to activate changes.'
